e38122f4a3685e7c390b1d2a60ebfc1370cf48b9
saad ahmed <saadahmed1742@gmail.com>
2022-11-21 18:35:26 +0500

    completed search bar, date , time

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./components/header"
 import { Header } from "./components/header"
 import "./App.css"
+import {SearchdateWise} from "./components/date"
+
 
 function App() {
   return (
     <div>
       <Header />
       <div className="background">
+        <SearchdateWise/>
       </div>
     </div>
   );
---
+++ src/components/date.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { DatePicker, Space } from "antd";
+import { TimePicker } from "antd";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import "../components/date.css"
+const onChange = (date, dateString) => {
+  console.log(date, dateString);
+};
+
+dayjs.extend(customParseFormat);
+const Time = (time, timeString) => {
+  console.log(time, timeString);
+};
+
+function SearchdateWise() {
+  return (
+    <div className="mainDiv">
+        <div className="searchDiv">
+      <div className="firstDiv">
+        <span>Where</span>
+        <input type="text" placeholder="City, airport, address or hotel" />
+      </div>
+      <div className="secondDiv">
+        <span>From</span>
+        <div>
+        <Space direction="vertical">
+          <DatePicker onChange={onChange} />
+        </Space>
+        <TimePicker
+          onChange={Time}
+          defaultOpenValue={dayjs("00:00:00", "HH:mm:ss")}
+        />
+        </div>
+      </div>
+      <div className="thirdDiv">
+        <span>Until</span>
+     <div>
+     <Space direction="vertical">
+        <DatePicker onChange={onChange} />
+      </Space>
+      <TimePicker
+        onChange={Time}
+        defaultOpenValue={dayjs("00:00:00", "HH:mm:ss")}
+      />
+     </div>
+      </div>
+      <div className="searchIcon">
+      <span><FontAwesomeIcon icon={faSearch} /></span>
+      </div>
+    </div>
+    </div>
+  );
+}
+export { SearchdateWise };
