5ddf924bc1aaf431fbc12dc7e65d8a58f2326400
saad ahmed <saadahmed1742@gmail.com>
2022-11-24 17:03:09 +0500

    completed Accordian Question and Footer Text

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,11 @@ import { Svgslider } from "./components/slider.jsx";
 import { ExperienceSlider } from "./components/slider.jsx";
 import { HostSlider } from "./components/slider.jsx";
 import { Image } from "./components/img";
-import {Button} from "./components/button"
+import { Button } from "./components/button";
+import { Accordian } from "./components/accordian";
+import { Footertext } from "./components/footer";
+
+
 function App() {
   return (
     <div>
@@ -34,49 +38,75 @@ function App() {
       <div className="fuel">
         <div className="fuel-Text">
           <div className="fuelText-h1">
-          <h1>Fuel your daydreams</h1>
+            <h1>Fuel your daydreams</h1>
           </div>
-          <span>Stoke your wanderlust with some dreamy photo chronicles of road trip adventures.</span>
+          <span>
+            Stoke your wanderlust with some dreamy photo chronicles of road trip
+            adventures.
+          </span>
           <div>
-          <Button title="Explore Travelogues" />
+            <Button title="Explore Travelogues" />
           </div>
         </div>
       </div>
       <div className="backgroundImage">
-        <img src="	https://blog.turo.com/blog/wp-content/uploads/2021/04/Kyle_Engman_21-copy-2_lesssmall.jpg" alt="" />
-      <div className="backgroundImage-Text">
-        <span>FEATURED TRAVELOGUE</span>
-        <h2>An Olympic experience in Washington</h2>
-        <p>Discover the epic waterfalls, moody weather, and lush forests of coastal Washington.</p>
-        <a href="#">Read more</a>
-      </div>
+        <img
+          src="	https://blog.turo.com/blog/wp-content/uploads/2021/04/Kyle_Engman_21-copy-2_lesssmall.jpg"
+          alt=""
+        />
+        <div className="backgroundImage-Text">
+          <span>FEATURED TRAVELOGUE</span>
+          <h2>An Olympic experience in Washington</h2>
+          <p>
+            Discover the epic waterfalls, moody weather, and lush forests of
+            coastal Washington.
+          </p>
+          <a href="#">Read more</a>
+        </div>
       </div>
       <div>
         <ExperienceSlider />
       </div>
-      <div className="host">
-      <div className="hostText">
-        <h1>Meet the hosts</h1>
-      </div>
+      <div className="hostess">
+        <div className="hostText">
+          <h1>Meet the hosts</h1>
+        </div>
       </div>
       <div>
         <HostSlider />
       </div>
       <div className="SvgImage">
         <div className="SvgImage-Div">
-        <Image />
-        <div className="book">
-          <a href="#">Book a car</a>
-          <br />
-          <span>Down the street or across the country, find the perfect vehicle for your next adventure.</span>
-        </div>
-        <div className="Host">
-          <a href="#">Become a host</a>
-          <br />
-          <span>Accelerate your entrepreneurship and start building a small car sharing business on Turo</span>
-        </div>
+          <Image />
+          <div className="book">
+            <a href="#">Book a car</a>
+            <br />
+            <span>
+              Down the street or across the country, find the perfect vehicle
+              for your next adventure.
+            </span>
+          </div>
+          <div className="Host">
+            <a href="#">Become a host</a>
+            <br />
+            <span>
+              Accelerate your entrepreneurship and start building a small car
+              sharing business on Turo
+            </span>
+          </div>
         </div>
       </div>
+        <div className="question">
+          <div className="questionText">
+            <h1>Frequently asked questions</h1>
+          </div>
+        </div>
+        <div>
+          <Accordian />
+        </div>
+        <div>
+          <Footertext />
+        </div>
     </div>
   );
 }
---
+++ src/components/accordian.jsx
@@ -0,0 +1,301 @@
+import React from "react";
+import "../components/accordian.css";
+const Accordian = () => {
+  return (
+    <div className="accordians">
+      <div class="container">
+        <div class="row">
+          <div class="col col-sm-12 col-md-6 col-xl-6 col-lg-6 accord">
+          <div class="accordion accordion-flush" id="accordionFlushExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                  <button
+                    class="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseOne"
+                    aria-expanded="true"
+                    aria-controls="panelsStayOpen-collapseOne"
+                  >
+                    What do I need to book a car on Turo?
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseOne"
+                  class="accordion-collapse collapse show"
+                  aria-labelledby="panelsStayOpen-headingOne"
+                >
+                  <div class="accordion-body">
+                    <strong>This is the first item's accordion body.</strong> It
+                    is shown by default, until the collapse plugin adds the
+                    appropriate classes that we use to style each element. These
+                    classes control the overall appearance, as well as the
+                    showing and hiding via CSS transitions. You can modify any
+                    of this with custom CSS or overriding our default variables.
+                    It's also worth noting that just about any HTML can go
+                    within the <code>.accordion-body</code>, though the
+                    transition does limit overflow.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 accord">
+          <div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingTwo">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="collapseTwo"
+                >
+                  What happens if I have an accident?
+                </button>
+              </h2>
+              <div
+                id="collapseTwo"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingTwo"
+                data-bs-parent="#accordionExample"
+              >
+                <div class="accordion-body">
+                  <strong>This is the second item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classes that we use to style each element. These
+                  classes control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the{" "}
+                  <code>.accordion-body</code>, though the transition does limit
+                  overflow.
+                </div>
+              </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 accord">
+          <div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingThree">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseThree"
+                  aria-expanded="false"
+                  aria-controls="collapseThree"
+                >
+                 Do I need my own insurance?
+                </button>
+              </h2>
+              <div
+                id="collapseThree"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingThree"
+                data-bs-parent="#accordionExample"
+              >
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classes that we use to style each element. These
+                  classes control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the{" "}
+                  <code>.accordion-body</code>, though the transition does limit
+                  overflow.
+                </div>
+              </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 accord">
+          <div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingFour">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseFour"
+                  aria-expanded="false"
+                  aria-controls="collapseThree"
+                >
+                  Can I get my car delivered to me?
+                </button>
+              </h2>
+              <div
+                id="collapseFour"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingThree"
+                data-bs-parent="#accordionExample"
+              >
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classes that we use to style each element. These
+                  classes control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the{" "}
+                  <code>.accordion-body</code>, though the transition does limit
+                  overflow.
+                </div>
+              </div>
+              </div>    
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 accord">
+          <div class="accordion accordion-flush" id="accordionFlushExample">
+          <div class="accordion-item">
+              <h2 class="accordion-header" id="headingFive">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseFive"
+                  aria-expanded="false"
+                  aria-controls="collapseFive"
+                >
+                  Can other people drive a car that I booked?
+                </button>
+              </h2>
+              <div
+                id="collapseFive"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingFive"
+                data-bs-parent="#accordionExample"
+              >
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classes that we use to style each element. These
+                  classes control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the{" "}
+                  <code>.accordion-body</code>, though the transition does limit
+                  overflow.
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 accord">
+          <div class="accordion accordion-flush" id="accordionFlushExample">
+          <div class="accordion-item">
+              <h2 class="accordion-header" id="headingSix">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseSix"
+                  aria-expanded="false"
+                  aria-controls="collapseSix"
+                >
+                  How do I get discounts when booking a car?
+                </button>
+              </h2>
+              <div
+                id="collapseSix"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingSix"
+                data-bs-parent="#accordionExample"
+              >
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classes that we use to style each element. These
+                  classes control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the{" "}
+                  <code>.accordion-body</code>, though the transition does limit
+                  overflow.
+                </div>
+              </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 accord">
+          <div class="accordion accordion-flush" id="accordionFlushExample">
+          <div class="accordion-item">
+              <h2 class="accordion-header" id="headingSeven">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseSeven"
+                  aria-expanded="false"
+                  aria-controls="collapseSeven"
+                >
+                  What is the cancellation policy on Turo?
+                </button>
+              </h2>
+              <div
+                id="collapseSeven"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingSeven"
+                data-bs-parent="#accordionExample"
+              >
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classes that we use to style each element. These
+                  classes control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the{" "}
+                  <code>.accordion-body</code>, though the transition does limit
+                  overflow.
+                </div>
+              </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 accord">
+          <div class="accordion accordion-flush" id="accordionFlushExample">
+          <div class="accordion-item">
+              <h2 class="accordion-header" id="headingEight">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseEight"
+                  aria-expanded="false"
+                  aria-controls="collapseEight"
+                >
+                  What are the cleaning and safety policies on Turo?
+                </button>
+              </h2>
+              <div
+                id="collapseEight"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingEight"
+                data-bs-parent="#accordionExample"
+              >
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classes that we use to style each element. These
+                  classes control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the{" "}
+                  <code>.accordion-body</code>, though the transition does limit
+                  overflow.
+                </div>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Accordian };
